e2e997d0d132ac195e48271b81f189519c15ee16
LaurenceZanotti <lau_zanotti@hotmail.com>
2022-04-02 15:57:03 -0300

    Update django.yml

--- capstone/settings.py
+++ capstone/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'capstone_db',
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASS'],
+        'USER': os.environ['DB_TEST_USER'] if os.environ.get('IS_CICD_TESTING') else os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_TEST_PASS'] if os.environ.get('IS_CICD_TESTING') else os.environ['DB_PASS'],
         'HOST':  os.environ['DB_HOST'] if os.environ.get('IS_CONTAINER') else '127.0.0.1',
         'PORT': '3306',
     }
