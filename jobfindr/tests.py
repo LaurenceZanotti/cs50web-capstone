a404934d6eaf0b01ff274ff006c9139792318a40
LaurenceZanotti <lau_zanotti@hotmail.com>
2022-04-02 14:52:49 -0300

    Selenium tests set up

--- jobfindr/tests.py
+++ jobfindr/tests.py
@@ -1,4 +1,7 @@
+from webbrowser import Chrome
 from django.test import TestCase, Client, LiveServerTestCase
+from selenium import webdriver
+from chromedriver_py import binary_path
 
 # Create your tests here.
 class IndexTests(TestCase):
@@ -6,3 +9,20 @@ class IndexTests(TestCase):
         c = Client()
         response = c.get("/")
         self.assertEqual(response.status_code, 200)
+
+class SeleniumIndexTests(LiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = webdriver.Chrome(binary_path)
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_index(self):
+        self.selenium.get(f'{self.live_server_url}/')
+        self.assertEqual(self.selenium.title, 'Jobfindr')
