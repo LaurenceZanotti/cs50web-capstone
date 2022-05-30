752d5ff7d02d659d7420b0720b642230b3263fc5
LaurenceZanotti <lau_zanotti@hotmail.com>
2022-05-30 17:45:00 -0300

    Nextjs+Django in separated containers

--- jobfindr/views.py
+++ jobfindr/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
 def index(request):
     if request.method == "GET":
         return render(request, "jobfindr/index.html")
+
+def hello(request):
+    if request.method == "GET":
+        return JsonResponse({'msg': 'Hello world! as JSON'})
