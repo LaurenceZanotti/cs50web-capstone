04d0d24612b05080b22ab609512bcecb2e8e63b8
Laurence Zanotti <lau_zanotti@hotmail.com>
2022-08-03 14:44:12 -0300

    Index / Welcome page navbar and hero section HTML

--- jobfindr_frontend/pages/index.js
+++ jobfindr_frontend/pages/index.js
@@ -1,10 +1,52 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import JobfindrLogo from "../components/JobFindrLogo/JobfindrLogo"
+import { UserCircle } from "phosphor-react"
 
 export default function Home({data}) {
   return (
-    <main>{data.msg}</main>
+    <>
+    <nav>
+      <div>
+        <h1>
+          <JobfindrLogo />
+        </h1>
+      </div>
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>Search talents</li>
+          <li>About</li>
+        </ul>
+      </div>
+      <div>
+        <UserCircle size={32} />
+      </div>
+    </nav>
+    <main>
+      {data.msg}
+      <section>
+        <h2>A <span>job</span> is looking for <span>you!</span></h2>
+        <div>
+          {/* Insert SVG icon and SVG background*/}
+        </div>
+        <form action="">
+          <div>
+            <p>More than <span>270,000</span> jobs are in the wait. Why not have a new opportunity?</p>
+          </div>
+          <div>
+            <div>
+              <input type="text" />
+              <input type="submit" value="Send" />
+            </div>
+            <div>or</div>
+            <div>
+              <a href="">Create profile</a>
+            </div>
+          </div>
+        </form>
+      </section>
+    </main>
+    <footer></footer>
+    </>
   )
 }
 
