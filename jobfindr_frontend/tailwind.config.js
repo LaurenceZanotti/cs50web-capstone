040f2c754510aca60e99c5935c7db487061bf749
Laurence Zanotti <lau_zanotti@hotmail.com>
2022-08-13 19:34:23 -0300

    Global font-family configuration

--- jobfindr_frontend/tailwind.config.js
+++ jobfindr_frontend/tailwind.config.js
@@ -1,4 +1,5 @@
 const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -8,6 +9,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'sans': ['Roboto', ...defaultTheme.fontFamily.sans]
+      },
       backgroundImage: {
         'hero-wave': "url('/index/Wave.svg')",
       }
