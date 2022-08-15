216b49ea987ac0c34bff370b5d6fcd9ad4d9b9fd
Laurence Zanotti <lau_zanotti@hotmail.com>
2022-08-15 20:34:44 -0300

    Fixed http-equiv and crossorigin meta tag warnings + Moved meta tags to _app.js to avoid a warning

--- jobfindr_frontend/pages/_document.js
+++ jobfindr_frontend/pages/_document.js
@@ -4,16 +4,8 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <title>Jobfindr</title>
-        <meta property="og:title" content="Jobfindr" key="title" />
-        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="Find jobs and talents, or both. You choose." />
-        <meta name="keywords" content="job, jobs, find, find jobs, search jobs, talents, talent, recruiting, opportunities" />
-
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet" />
       </Head>
       <body>
