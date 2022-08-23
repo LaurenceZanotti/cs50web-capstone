29d895bcbc465d2bba87950e594d533bbc57d1f2
Laurence Zanotti <lau_zanotti@hotmail.com>
2022-08-22 22:54:56 -0300

    Auth aside blended background image

--- jobfindr_frontend/components/login/AuthLayout.jsx
+++ jobfindr_frontend/components/login/AuthLayout.jsx
@@ -5,8 +5,7 @@ export default function AuthLayout(props) {
         <main className="w-full sm:block sm:w-6/12 bg-gray-200">
             {props.children}
         </main>
-        <aside className="w-0 sm:block sm:w-6/12 bg-primary/[.6]">
-            {/* <div className="w-full h-full bg-cover bg-no-repeat bg-right bg-woman-working"></div> */}
+        <aside className="w-0 sm:block sm:w-6/12 bg-primary/[.6] bg-woman-working bg-blend-soft-light bg-cover bg-right">
         </aside>
     </div>
   )
