0f4902c0e76344f4e316df2fd05f7503d8fadfeb
Laurence Zanotti <lau_zanotti@hotmail.com>
2022-08-22 23:47:30 -0300

    Register page components and CSS

--- jobfindr_frontend/components/login/InputSubmit.jsx
+++ jobfindr_frontend/components/login/InputSubmit.jsx
@@ -1,8 +1,12 @@
-export default function InputSubmit({value}) {
+export default function InputSubmit({value, color}) {
   return (
-    <div className="cursor-pointer my-4 text-white bg-primary flex rounded-full justify-center items-center">
+    <div className={`cursor-pointer my-4 text-white bg-${color} flex rounded-full justify-center items-center`}>
       <input type="submit" value={value} className="h-10 cursor-pointer"/>
     </div>
   )
 }
 
+InputSubmit.defaultProps = {
+  value: '',
+  color: 'primary'
+}
--- jobfindr_frontend/pages/register.js
+++ jobfindr_frontend/pages/register.js
@@ -2,7 +2,7 @@
 import Head from 'next/head'
 
 // Libraries
-import { UserCircle, LockSimple } from 'phosphor-react'
+import { UserCircle, LockSimple, Envelope, Repeat } from 'phosphor-react'
 
 // Icons
 import GoogleIcon from "../components/login/icons/GoogleIcon";
@@ -15,11 +15,13 @@ import AuthHeader from "../components/login/AuthHeader";
 import InputText from "../components/login/InputText";
 import InputSubmit from "../components/login/InputSubmit";
 
-export default function Login() {
+
+
+export default function Register() {
   return (
     <>
     <Head>
-        <title>Log in | Jobfindr</title>
+        <title>Register | Jobfindr</title>
     </Head>
     <div>
         <AuthLayout>
@@ -32,21 +34,34 @@ export default function Login() {
                     placeholder="Username"
                 />
                 <InputText 
+                    icon={<Envelope size={28} color="black" weight="fill" className='mx-4'/>} 
+                    name="email"
+                    id="email"
+                    type="email"
+                    placeholder="Email"
+                />
+                <InputText 
                     icon={<LockSimple size={28} color="black" weight="fill" className='mx-4'/>} 
                     name="password"
                     id="password"
                     type="password"
                     placeholder="Password"
                 />
-                <InputSubmit value="Log in"/>
+                <InputText 
+                    icon={<Repeat size={28} color="black" weight="fill" className='mx-4'/>} 
+                    name="cpassword"
+                    id="cpassword"
+                    type="password"
+                    placeholder="Confirm your password"
+                />
+                <InputSubmit value="Register" color="secondary-500" />
             </form>
             <div className='text-center mt-0 mb-10 leading-8'>
-                <p>I forgot my <a href="" className="text-primary font-medium">password</a></p>
-                <p>Don't have an account?
-                    <a href="/register" className='text-secondary-500 font-medium ml-1'>Create one</a>
+                <p>Already have an account?
+                    <a href="/login" className='text-secondary-500 font-medium ml-1'>Sign in</a>
                 </p>
                 <p>or</p>
-                <p>Continue with a social network</p>
+                <p>Register using a social network</p>
             </div>
             <div className='flex justify-center gap-x-14'>
                 <a href="">
