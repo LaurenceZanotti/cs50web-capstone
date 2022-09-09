cd5da246f82f984156b7fe2aaaf1bc155a65d15f
Laurence Zanotti <lau_zanotti@hotmail.com>
2022-09-09 14:25:28 -0300

    API tests for signin up a Talent Hunter user and a Job Seeker user

--- jobfindr/tests_api.py
+++ jobfindr/tests_api.py
@@ -9,3 +9,62 @@ class IndexTests(TestCase):
         c = Client()
         response = c.get("/api/")
         self.assertEqual(response.status_code, 200)
+
+class AuthTests(TestCase):
+    target_url = "/api/register"
+
+    def test_register_jobseeker(self):
+        """Tests the sign up of a Job Seeker user"""
+        c = Client()
+        response = c.post(self.target_url, {
+            'username': 'dine',
+            'email': 'dine@test.com',
+            'password': 'secret',
+            'cpassword': 'secret',
+            'usertype': 'jobseeker'
+        })
+
+        # Test if resource was created by status code
+        self.assertEqual(
+            201, 
+            response.status_code, 
+            msg="Status code not correct"
+        )
+
+        # Test if JSON response is correct and if user was created
+        # https://stackoverflow.com/questions/27472663/
+        # how-to-use-djangos-assertjsonequal-to-verify-response-of-view-returning-jsonres
+        self.assertJSONEqual(
+            str(response.content, encoding='utf8'),
+            {
+                'msg': 'User created!'
+            }
+        )
+
+    def test_register_talenthunter(self):
+        """Tests the sign up of a Talent Hunter user"""
+        c = Client()
+        response = c.post(self.target_url, {
+            'username': 'emilly',
+            'email': 'emilly@test.com',
+            'password': 'secret',
+            'cpassword': 'secret',
+            'usertype': 'talenthunter'
+        })
+
+        # Test if resource was created by status code
+        self.assertEqual(
+            201, 
+            response.status_code, 
+            msg="Status code not correct"
+        )
+
+        # Test if JSON response is correct and if user was created
+        # https://stackoverflow.com/questions/27472663/
+        # how-to-use-djangos-assertjsonequal-to-verify-response-of-view-returning-jsonres
+        self.assertJSONEqual(
+            str(response.content, encoding='utf8'),
+            {
+                'msg': 'User created!'
+            }
+        )
