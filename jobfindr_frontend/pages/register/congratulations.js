63313b8e7a2a83135be9bd065eb460c2f1541b21
Laurence Zanotti <lau_zanotti@hotmail.com>
2022-09-16 19:47:14 -0300

    Congratulations page after sign up with redirect counter

--- jobfindr_frontend/pages/register/congratulations.js
+++ jobfindr_frontend/pages/register/congratulations.js
@@ -1,7 +1,8 @@
-import { useState, useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 // Built in
 import Head from 'next/head'
+import Router from 'next/router'
 
 // Components
 import AuthLayout from "../../components/login/AuthLayout";
@@ -9,6 +10,11 @@ import AuthHeader from "../../components/login/AuthHeader";
 
 export default function Congratulations() {
     const [countDown, setCountDown] = useState(7)
+
+    useEffect(() => {
+        if (countDown == 1) Router.push("/login")
+        countDown > 1 && setTimeout(() => setCountDown(countDown - 1), 1000)
+    }, [countDown])
 
     return (
         <>
@@ -22,7 +28,7 @@ export default function Congratulations() {
                     <p className='text-secondary-500 font-medium'>Congratulations ✨🎉</p>
                     <p>You are ready to start! Now you can <a href="/login" className='text-primary font-medium'>sign in</a></p>
                     <p>Redirecting in {countDown} seconds...</p>
-                </div>            
+                </div>
             </AuthLayout>
         </div>
         </>
