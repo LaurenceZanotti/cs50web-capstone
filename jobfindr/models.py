93bb6e16ab15b283eadc3510399a769e30163829
Laurence Zanotti <lau_zanotti@hotmail.com>
2022-09-28 13:16:56 -0300

    Fullname default value is set by overriden save() method on models layer

--- jobfindr/models.py
+++ jobfindr/models.py
@@ -19,10 +19,31 @@ class Profile(models.Model):
     def user_directory_path(self, filename):
         return f'pros/{self.user.id}/{filename}'
 
+    def save(self, *args, **kwargs):
+        """
+        Overidden save method to fill fullname field
+        
+        Save the current instance. Override this in a subclass if you want to
+        control the saving process.
+
+        The 'force_insert' and 'force_update' parameters can be used to insist
+        that the "save" must be an SQL insert or update (or equivalent for
+        non-SQL backends), respectively. Normally, they should not be set.
+        """
+        # Fill fullname field if it is None
+        # https://stackoverflow.com/questions/44421024
+        # /django-model-field-with-default-value-from-another-model
+        if self.fullname is None:
+            self.fullname =  f'{self.owner.first_name} {self.owner.last_name}' \
+                if self.owner.first_name and self.owner.last_name else \
+                self.owner.username
+        super(Profile, self).save(*args, **kwargs)
+
+
     # Fields
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_picture = models.ImageField(upload_to=user_directory_path)
-    fullname = models.CharField(max_length=254)
+    fullname = models.CharField(max_length=254, null=True, blank=True)
     title = models.CharField(max_length=128, null=True, blank=True)
     about = models.TextField(max_length=1024, null=True, blank=True)
     # https://docs.djangoproject.com/en/4.1/ref/models/fields/#jsonfield
